84f22e7cd26e10d18ba31bc9d0747f479f53219f
Locallize <Fillanlee@aol.com>
2019-05-16 18:25:21 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,32 @@
 // Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component{
+    state= {
+        clicked: false,
+        count: 0
+    }
+
+    clickHandle = (e)=>{
+        // console.log('clciking')
+        this.setState({
+            clicked: true,
+            count: this.state.count +1
+        })
+
+    }
+
+    render(){
+        // console.log(this.state.count)
+        return (<div>
+            hellow
+            <button onClick={(e)=>this.clickHandle(e)}>
+        clcik me
+            </button>
+            <p>I've been click {this.state.clicked && this.state.count === 20 ? 'relaxx..... ': this.state.clicked ? "yes": 'nvmm'}</p>
+            <p>clicked {this.state.count} times</p>
+        </div>)
+    }
+}
+
+export default ClickityClick
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClcik from './components/ClickityClick'
 
 ReactDOM.render(
   <div>
     Mount Components Here
+    <ClickityClcik/>
   </div>,
   document.getElementById('root')
 );
